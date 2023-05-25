1d9b1eb5413558b96d550bff9ecc6aad81f8be8a
Maheshkumar N <pn.maheshkumar@gmail.com>
2023-05-25 13:02:17 +0530

    Basic

--- GenericsClientConApp/Program.cs
+++ GenericsClientConApp/Program.cs
@@ -1,8 +1,5 @@
 ﻿using static System.Console;
 
-using GenericsLibrary;
-using System.Reflection;
-using System.Runtime.CompilerServices;
 using GenericsClientConApp;
 
 
@@ -13,27 +10,4 @@ using GenericsClientConApp;
 
 
 int num1 = 10, num2 = 20;
-SwapTool swapTool = new SwapTool();
-//WriteLine($"Before Swap Num1: {num1} and Num2: {num2}");
-//swapTool.Swap(num1, num2);
-//WriteLine($"After Swap Num1: {num1} and Num2: {num2}");
-
-
-//WriteLine($"Ref Before Swap Num1: {num1} and Num2: {num2}");
-//swapTool.Swap(num1, num2);
-//WriteLine($"Ref After Swap Num1: {num1} and Num2: {num2}");
-
-string firstName = "Peter", lastName = "Parker";
-//WriteLine($"Ref Before Swap FirstName: {firstName} and LastName: {lastName}");
-//swapTool.Swap(ref firstName, ref lastName);
-//WriteLine($"Ref After Swap FirstName: {firstName} and LastName: {lastName}");
-
-//Printer.PrintHeader($"Ref Before Swap FirstName: {firstName} and LastName: {lastName}");
-//swapTool.Swap(ref firstName, ref lastName);
-//WriteLine($"FirstName: {firstName} and LastName: {lastName}");
-//Printer.PrintFooter();
-
-
-Printer.Print($"FirstName: {firstName} and LastName: {lastName}", "Before Swap");
-swapTool.Swap(ref firstName, ref lastName);
-Printer.Print($"FirstName: {firstName} and LastName: {lastName}", "After Swap");
+WriteLine($"Num1: {num1} and Num2: {num2}");
