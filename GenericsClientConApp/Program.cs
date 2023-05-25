f20de558a17701d9646cd4159659948756d665c3
Maheshkumar N <pn.maheshkumar@gmail.com>
2023-05-25 14:56:58 +0530

    Generic Array

--- GenericsClientConApp/Program.cs
+++ GenericsClientConApp/Program.cs
@@ -12,8 +12,9 @@ using GenericsClientConApp;
 #endregion
 
 
-int num1 = 10, num2 = 20;
-SwapTool swapTool = new SwapTool();
+#region SwapTool
+//int num1 = 10, num2 = 20;
+//SwapTool swapTool = new SwapTool();
 //WriteLine($"Before Swap Num1: {num1} and Num2: {num2}");
 //swapTool.Swap(num1, num2);
 //WriteLine($"After Swap Num1: {num1} and Num2: {num2}");
@@ -23,7 +24,7 @@ SwapTool swapTool = new SwapTool();
 //swapTool.Swap(num1, num2);
 //WriteLine($"Ref After Swap Num1: {num1} and Num2: {num2}");
 
-string firstName = "Peter", lastName = "Parker";
+//string firstName = "Peter", lastName = "Parker";
 //WriteLine($"Ref Before Swap FirstName: {firstName} and LastName: {lastName}");
 //swapTool.Swap(ref firstName, ref lastName);
 //WriteLine($"Ref After Swap FirstName: {firstName} and LastName: {lastName}");
@@ -34,6 +35,48 @@ string firstName = "Peter", lastName = "Parker";
 //Printer.PrintFooter();
 
 
-Printer.Print($"FirstName: {firstName} and LastName: {lastName}", "Before Swap");
-swapTool.Swap(ref firstName, ref lastName);
-Printer.Print($"FirstName: {firstName} and LastName: {lastName}", "After Swap");
+//Printer.Print($"FirstName: {firstName} and LastName: {lastName}", "Before Swap");
+//swapTool.Swap(ref firstName, ref lastName);
+//Printer.Print($"FirstName: {firstName} and LastName: {lastName}", "After Swap"); 
+#endregion
+
+#region With GenArray<int>
+//GenArray<int> genArray = new GenArray<int>();
+//genArray.Add(10);
+//genArray.Add(2);
+
+//Printer.Print($"{genArray[1]}", "genArray[1]");
+////Printer.Print($"{genArray[2]}", "genArray[2]");
+//genArray.Add(7);
+//genArray.Add(9);
+
+//Printer.PrintHeader("Loop through the Array");
+//for (int i = 0; i < genArray.Length; i++)
+//{
+//  WriteLine($"genArray[{i}] = {genArray[i]}");
+//}
+//Printer.PrintFooter();
+
+#endregion
+
+
+#region GenArray<string>
+GenArray<string> genArray = new GenArray<string>();
+genArray.Add("Tintin");
+genArray.Add("Mickey");
+
+Printer.Print($"{genArray[1]}", "genArray[1]");
+//Printer.Print($"{genArray[2]}", "genArray[2]");
+genArray.Add("Jerry");
+genArray.Add("Phantom");
+
+Printer.PrintHeader("Loop through the Array");
+for (int i = 0; i < genArray.Length; i++)
+{
+	WriteLine($"genArray[{i}] = {genArray[i]}");
+}
+Printer.PrintFooter();
+
+
+#endregion
+
---
+++ GenericsLibrary/GenArray.cs
@@ -0,0 +1,62 @@
+﻿namespace GenericsLibrary
+{
+  public class GenArray<T>
+  {
+    int _arraySize;
+    int _position;
+    T[] _array;
+
+    public GenArray()
+    {
+      _arraySize = 3;
+      _position = 0;
+      _array = new T[_arraySize];
+    }
+
+    public int Length { get { return _position; } }
+
+    public T this[int position]
+    {
+      get
+      {
+        if (position >= _position)
+        {
+          throw new IndexOutOfRangeException($"No data found at the position: '{position}'");
+        }
+        return _array[position];
+      }
+    }
+
+    public void Add(T value)
+    {
+      _array[_position++] = value;
+      if (_position == _arraySize)
+      {
+        _arraySize += 3;
+        T[] temp = new T[_arraySize];
+        for (int i = 0; i < _position; i++)
+        {
+          temp[i] = _array[i];
+        }
+        _array = temp;
+      }
+    }
+
+    //public void Sort()
+    //{
+    //  for (int i = 0; i < _position; i++)
+    //  {
+    //    for (int j = (i+1); j < _position; j++)
+    //    {
+    //      if (_array[i] > _array[i]) // Will have to use Constraint
+    //      {
+    //        T temp = _array[i];
+    //        _array[i] = _array[j];
+    //        _array[j] = temp;
+    //      }
+    //    }
+    //  }
+    //}
+
+  }
+}
