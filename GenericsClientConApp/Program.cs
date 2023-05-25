ec09b08da0b59b27f902177ebad049ec7ff6dd9c
Maheshkumar N <pn.maheshkumar@gmail.com>
2023-05-25 12:54:54 +0530

    Add project files.

---
+++ GenericsClientConApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using static System.Console;
+
+using GenericsLibrary;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using GenericsClientConApp;
+
+
+#region Default Code
+//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+#endregion
+
+
+int num1 = 10, num2 = 20;
+SwapTool swapTool = new SwapTool();
+//WriteLine($"Before Swap Num1: {num1} and Num2: {num2}");
+//swapTool.Swap(num1, num2);
+//WriteLine($"After Swap Num1: {num1} and Num2: {num2}");
+
+
+//WriteLine($"Ref Before Swap Num1: {num1} and Num2: {num2}");
+//swapTool.Swap(num1, num2);
+//WriteLine($"Ref After Swap Num1: {num1} and Num2: {num2}");
+
+string firstName = "Peter", lastName = "Parker";
+//WriteLine($"Ref Before Swap FirstName: {firstName} and LastName: {lastName}");
+//swapTool.Swap(ref firstName, ref lastName);
+//WriteLine($"Ref After Swap FirstName: {firstName} and LastName: {lastName}");
+
+//Printer.PrintHeader($"Ref Before Swap FirstName: {firstName} and LastName: {lastName}");
+//swapTool.Swap(ref firstName, ref lastName);
+//WriteLine($"FirstName: {firstName} and LastName: {lastName}");
+//Printer.PrintFooter();
+
+
+Printer.Print($"FirstName: {firstName} and LastName: {lastName}", "Before Swap");
+swapTool.Swap(ref firstName, ref lastName);
+Printer.Print($"FirstName: {firstName} and LastName: {lastName}", "After Swap");
---
+++ README.md
@@ -0,0 +1,18 @@
+# Generics Demo using .Net 6.0
+
+### Projects in the Solution
+
+- Solution
+    - GenericsSolution
+        - GenericsClientConApp (.Net 6 Console Application)
+        - GenericsLibrary (.Net 6 Class Library)
+
+#### Branches List
+
+- 01Basics
+- 02SwapTool
+- 03GenericSwap
+- 04GenericArray
+- 05Constraint
+- 06PersonClass
+- 07GenericDelegate
